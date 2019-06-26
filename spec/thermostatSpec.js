dd44357d70f5f7e1e80577b4e63404d73c456edd
Student <kaczanowskamonika@gmail.com>
2019-06-26 15:33:48 +0100

    temp increases with up function

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -3,12 +3,16 @@
 describe("Thermostat", function() {
 
   let thermostat;
-  
+
   beforeEach(function(){
     thermostat = new Thermostat();
   });
 
   it("starts at 20 degrees", function() {
     expect(thermostat.defaultTemperature()).toEqual(20);
+  });
+
+  it("increases the temperature with up function", function() {
+    expect(thermostat.up()).toEqual(21);
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,7 +2,15 @@ function Thermostat() {
   this.defaultTemperature = function() {
       return 20;
   };
-}
+
+  this.temp = 20
+
+  this.up = function(){
+    this.temp ++
+    return this.temp
+  };
+
+};
 
 //Thermostat.prototype.defaultTemperature = function() {
  // return 20;
